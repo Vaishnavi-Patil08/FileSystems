962c9c146fe05b9c6e878979ba3ba5b46e92fdd0
Vaishnavi Patil <vpati014@xe-07.cs.ucr.edu>
2025-03-15 17:09:47 -0700

    Symbolic Links

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -286,6 +286,27 @@ create(char *path, short type, short major, short minor)
 uint64
 sys_symlink(void)
 {
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op(ROOTDEV);
+
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op(ROOTDEV);
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, strlen(target) + 1) != strlen(target) + 1) {
+    iunlockput(ip);
+    end_op(ROOTDEV);
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op(ROOTDEV);
   //your implementation goes here
   return 0;
 }
@@ -316,6 +337,32 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+
+    int link_count = 0;
+    char target[MAXPATH];
+    while(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+      if(link_count++ > 10) {
+        iunlockput(ip);
+        end_op(ROOTDEV);
+        return -1;
+      }
+
+      memset(target, 0, MAXPATH);
+      if(readi(ip, 0, (uint64)target, 0, MAXPATH) <= 0) {
+        iunlockput(ip);
+        end_op(ROOTDEV);
+        return -1;
+      }
+
+      iunlockput(ip);
+      
+      if((ip = namei(target)) == 0) {
+        end_op(ROOTDEV);
+        return -1;
+      }
+      ilock(ip);
+    }
+
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op(ROOTDEV);
@@ -351,7 +398,7 @@ sys_open(void)
 
   iunlock(ip);
   end_op(ROOTDEV);
-
+  
   return fd;
 }
 
